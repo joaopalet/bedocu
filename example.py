834e2d0c8945058d7f168152f30c8e2b9fdbcfae
João Palet <joao.palet@outlook.com>
2024-04-05 19:57:23 +0100

    Add example project; update example.py to read one file and summarize it

--- example.py
+++ example.py
@@ -2,7 +2,12 @@ from langchain_google_vertexai import VertexAI
 
 model = VertexAI(model_name="gemini-pro")
 
-message = "What are some of the pros and cons of Python as a programming language?"
+f = open("./example-project/website-builder/app/__init__.py", "r")
+contents = f.read()
+message = (
+    "This is the contents of a file. Based on it, generate a summary of its functionality. File: "
+    + contents
+)
 res = model.invoke(message)
 
 print(res)
