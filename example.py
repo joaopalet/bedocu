566f16b6884d99922d19c5254facdb19c9f97583
João Palet <joao.palet@outlook.com>
2024-04-05 20:14:35 +0100

    Add agent to summarize single file

--- example.py
+++ example.py
@@ -2,12 +2,22 @@ from langchain_google_vertexai import VertexAI
 
 model = VertexAI(model_name="gemini-pro")
 
-f = open("./example-project/website-builder/app/__init__.py", "r")
-contents = f.read()
-message = (
-    "This is the contents of a file. Based on it, generate a summary of its functionality. File: "
-    + contents
-)
-res = model.invoke(message)
+file_num_limit = 50
+file_lines_limit = 200
 
+processed_files_count = 0
+
+
+def getFileSummary(filepath):
+    f = open(filepath, "r")
+    contents = f.read()
+    message = (
+        "This is the contents of a file. Based on it, generate a summary of its functionality. File: "
+        + contents
+    )
+    res = model.invoke(message)
+    return res
+
+
+res = "./example-project/website-builder/app/__init__.py"
 print(res)
